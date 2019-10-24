140cd3804e14fe5df608b3d6b911ea0347862f06
oldshiji <1655664358@qq.com>
2019-10-24 19:12:13 +0800

    cmake 安装，cmake静态库动态库生成和引用

---
+++ tm4_test/src/main.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <hello.h>
+
+int main(void)
+{
+	helloFunc();
+
+	return 0;
+}
---
+++ tml2/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+	printf("hello world from tm1 main!\n");
+	return 0;
+}
